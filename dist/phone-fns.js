d352c1aa45a4adedbf067683cf2080b399b9ceff
dhershman1 <dustinh17@gmail.com>
2018-06-29 17:18:07 -0400

    new build files

--- dist/phone-fns.js
+++ dist/phone-fns.js
@@ -58,7 +58,7 @@
     return phone.replace(/[a-z]\w?|\W/gi, '');
   });
 
-  var breakdown = curry(function (countryCode, phone) {
+  var breakdown = function breakdown(countryCode, phone) {
     var _uglify$match = uglify(phone).match(/([0-9]{3})?([0-9]{3})([0-9]{4})([0-9]{1,})?/),
         _uglify$match2 = _slicedToArray(_uglify$match, 5),
         areaCode = _uglify$match2[1],
@@ -73,71 +73,71 @@
       lineNumber: lineNumber,
       extension: extension
     };
-  });
+  };
+  var breakdown$1 = curry(breakdown);
 
-  var find = curry(function (type, phone) {
-    return breakdown('', phone)[type];
-  });
+  var find = function find(type, phone) {
+    return breakdown$1('', phone)[type];
+  };
+  var find$1 = curry(find);
 
   var isValid = (function (phone) {
     var uglyPhone = uglify(phone);
-    if (!phone || uglyPhone.length < 10) {
+    if (!phone || uglyPhone.length < 7) {
       return false;
     }
-    var _breakdown = breakdown('', uglyPhone),
+    var _breakdown = breakdown$1('', uglyPhone),
         areaCode = _breakdown.areaCode,
         localCode = _breakdown.localCode,
         lineNumber = _breakdown.lineNumber;
-    return phone && /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(areaCode + localCode + lineNumber);
+    if (uglyPhone.length === 7) {
+      return /^([0-9]{3})[-. ]?([0-9]{4})$/.test(localCode + lineNumber);
+    }
+    return /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(areaCode + localCode + lineNumber);
   });
 
-  var replaceLayout = function replaceLayout(layout, num, type) {
-    var letters = {
-      areaCode: 'A',
-      localCode: 'L',
-      lineNumber: 'N',
-      extension: 'E',
-      countryCode: 'C'
-    };
-    var results = layout.concat();
-    num.split('').forEach(function (n) {
-      results = results.replace(letters[type], n);
-    });
-    return results;
+  var validFormat = function validFormat(phone, layout) {
+    var count = layout.split('').reduce(function (acc, v) {
+      if (v.toLowerCase() === 'n') {
+        acc++;
+      }
+      return acc;
+    }, 0);
+    return count === phone.length;
   };
-  var format = curry(function (countryCode, layout, phone) {
-    if (!isValid(phone)) {
+  var format = function format(cc, layout, phone) {
+    var fullPhone = "".concat(cc).concat(uglify(phone));
+    if (!isValid(phone) || !validFormat(fullPhone, layout)) {
       return phone;
     }
-    var uglyPhone = uglify(phone);
-    var phoneObj = breakdown(countryCode, uglyPhone);
-    var results = layout;
-    for (var prop in phoneObj) {
-      if (phoneObj[prop]) {
-        results = replaceLayout(results, phoneObj[prop], prop);
-      }
-    }
+    var results = fullPhone.split('').reduce(function (acc, d) {
+      var temp = acc.replace(/N/i, d);
+      return temp;
+    }, layout);
     return results;
-  });
+  };
+  var format$1 = curry(format);
 
-  var match = curry(function (x, y) {
+  var match = function match(x, y) {
     if (!isValid(x) || !isValid(y)) {
       return false;
     }
     return uglify(x) === uglify(y);
-  });
+  };
+  var match$1 = curry(match);
 
   var phoneFns = function phoneFns() {
     var countryCode = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-    if (typeof countryCode !== 'string') {
-      throw new TypeError('Country Code needs to be a string');
+    if (typeof countryCode !== 'string' && typeof countryCode !== 'number') {
+      throw new TypeError('Country Code needs to be a string or number');
     }
+    var ccStr = String(countryCode);
     return {
-      breakdown: breakdown(countryCode),
-      format: format(countryCode),
-      find: find(countryCode),
+      breakdown: breakdown$1(ccStr),
+      format: format$1(ccStr),
+      find: find$1(ccStr),
       isValid: isValid,
-      match: match,
+      match: match$1,
       uglify: uglify
     };
   };
