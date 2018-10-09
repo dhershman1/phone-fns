108ac32984276c9d9df2af6a87325f9a7ce671bd
dhershman1 <dustinh17@gmail.com>
2018-10-09 17:08:01 -0400

    Re configured how the main index works

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-import breakdown from './breakdown'
-import find from './find'
-import format from './format'
-import isValid from './isValid'
-import match from './match'
-import uglify from './uglify'
-
-const phoneFns = (countryCode = '') => {
-  if (typeof countryCode !== 'string' && typeof countryCode !== 'number') {
-    throw new TypeError('Country Code needs to be a string or number')
-  }
-
-  const ccStr = String(countryCode)
-
-  return {
-    breakdown: breakdown(ccStr),
-    format: format(ccStr),
-    find: find(ccStr),
-    isValid,
-    match,
-    uglify
-  }
-}
-
-export default phoneFns
+export { default as breakdown } from './breakdown'
+export { default as format } from './format'
+export { default as isValid } from './isValid'
+export { default as uglify } from './uglify'
