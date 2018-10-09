d7da861334a8eab4b489b319a1081378531bb58e
dhershman1 <dustinh17@gmail.com>
2018-10-09 17:07:16 -0400

    removed the need of cc from breakdown

--- src/breakdown.js
+++ src/breakdown.js
@@ -1,4 +1,3 @@
-import curry from './_internals/curry'
 import uglify from './uglify'
 
 /**
@@ -11,26 +10,20 @@ import uglify from './uglify'
  * @return {Object} Returns an object of the broken down phone number
  *
  * @example
- * breakdown('', '111-222-3333');
- * // => { countryCode: '', areaCode: '111', localCode: '222', lineNumber: '3333', extension: '' }
+ * breakdown('111-222-3333');
+ * // => { areaCode: '111', localCode: '222', lineNumber: '3333', extension: '' }
  *
- * breakdown('1787', '5554441111');
- * // => { countryCode: 1787, areaCode: '555', localCode: '444', lineNumber: '1111', extension: '' }
+ * breakdown('5554441111');
+ * // => { areaCode: '555', localCode: '444', lineNumber: '1111', extension: '' }
  *
  * breakdown('', '555-444-3333 x 8989');
- * // => { countryCode: '', areaCode: '555', localCode: '444', lineNumber: '3333', extension: 8989 }
- *
- * // Breakdown is curried
- * const breaker = breakdown('');
- *
- * breaker('222-333-4444'); // => { areaCode: '222', localCode: '333', lineNumber: '4444' }
+ * // => { areaCode: '555', localCode: '444', lineNumber: '3333', extension: 8989 }
  */
-const breakdown = (countryCode, phone) => {
+const breakdown = phone => {
   const [, areaCode, localCode, lineNumber, extension = ''] = uglify(phone)
     .match(/([0-9]{3})?([0-9]{3})([0-9]{4})([0-9]{1,})?/)
 
   return {
-    countryCode,
     areaCode,
     localCode,
     lineNumber,
@@ -38,4 +31,4 @@ const breakdown = (countryCode, phone) => {
   }
 }
 
-export default curry(breakdown)
+export default breakdown
--- tests/breakdown.js
+++ tests/breakdown.js
@@ -1,23 +1,20 @@
 import breakdown from '../src/breakdown'
 import test from 'tape'
 
-const breaker = breakdown('')
-
 test('Test breakdown normal', t => {
-  const results = breaker('555-444-3333')
+  const results = breakdown('555-444-3333')
 
   t.ok(results, 'results came back')
   t.is(typeof results, 'object', 'results came back as an object')
   t.is(results.areaCode, '555', 'The area code in results was 555')
   t.is(results.localCode, '444', 'The local code in results was 444')
   t.is(results.lineNumber, '3333', 'The lineNumber in results was 3333')
-  t.notOk(results.countryCode.length, 'There was no country code')
   t.notOk(results.extension.length, 'There was no extension')
   t.end()
 })
 
 test('Test breakdown extension', t => {
-  const results = breaker('555-444-3333 x 8989')
+  const results = breakdown('555-444-3333 x 8989')
 
   t.ok(results, 'results came back')
   t.is(typeof results, 'object', 'results came back as an object')
@@ -25,19 +22,17 @@ test('Test breakdown extension', t => {
   t.is(results.localCode, '444', 'The local code in results was 444')
   t.is(results.lineNumber, '3333', 'The lineNumber in results was 3333')
   t.is(results.extension, '8989', 'The extension number was 8989')
-  t.notOk(results.countryCode.length, 'There was no country code')
   t.end()
 })
 
-test('Test breakdown long distance', t => {
-  const results = breakdown('1787', '5554441111')
+test('Test breakdown another normal', t => {
+  const results = breakdown('5554441111')
 
   t.ok(results, 'results came back')
   t.is(typeof results, 'object', 'results came back as an object')
   t.is(results.areaCode, '555', 'The area code in results was 555')
   t.is(results.localCode, '444', 'The local code in results was 444')
   t.is(results.lineNumber, '1111', 'The lineNumber in results was 3333')
-  t.is(results.countryCode, '1787', 'The country code was 1-787')
   t.notOk(results.extension.length, 'The extension number was empty')
   t.end()
 })
