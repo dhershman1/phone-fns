7c4441265d24c8ed195fb41ccc222f16438a3c80
dhershman1 <dustinh17@gmail.com>
2018-11-15 09:46:36 -0500

    Cleaned up internal scripts

--- scripts/create-export.js
+++ scripts/create-export.js
@@ -4,18 +4,6 @@ const globby = require('globby')
 
 const files = globby.sync(['src/**/*.js', '!src/index.js', '!src/common.js', '!src/_internals'])
 
-// const propertyRequireLines = globby.sync(['src/*.js', '!src/index.js'])
-//   .map(file => {
-//     const { base, name } = path.parse(file)
-
-//     return `export { default as ${name} } from './${base}'`
-//   })
-// const indexLines = []
-//   .concat(propertyRequireLines.join('\n'))
-//   .join('\n')
-
-// fs.writeFileSync('src/index.js', `${indexLines}\n`)
-
 fs.writeFileSync('./index.js', `${files.map(f => {
   const { dir, base, name } = path.parse(f)
 
