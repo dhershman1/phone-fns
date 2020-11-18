257a3e7ae0428cad5380b61b488001d7c62116df
Dustin Hershman <dustinh17@gmail.com>
2020-11-18 14:52:38 -0500

    auto commit [ci skip]

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as breakdown } from './breakdown.js'
-export { default as format } from './format.js'
-export { default as isValid } from './isValid.js'
-export { default as uglify } from './uglify.js'
---
+++ static/js/search.js
@@ -0,0 +1,50 @@
+const search = document.getElementById('filter')
+const navItems = Array.from(document.querySelectorAll('.nav__item'))
+
+/* eslint-disable no-labels */
+
+/* global hljs */
+
+hljs.initHighlightingOnLoad()
+
+// FuzzySearch taken directly from kyanite!
+const fuzzySearch = (needle, haystack) => {
+  const hLen = haystack.length
+  const nLen = needle.length
+  let j = 0
+
+  if (nLen > hLen) {
+    return false
+  }
+
+  if (nLen === hLen) {
+    return needle === haystack
+  }
+
+  outer: for (let i = 0; i < nLen; i++) {
+    const nChar = needle.charCodeAt(i)
+
+    for (j; j < hLen; j++) {
+      if (haystack.charCodeAt(j) === nChar) {
+        continue outer
+      }
+    }
+
+    return false
+  }
+
+  return true
+}
+
+search.oninput = ({ target }) => {
+  const val = target.value.toLowerCase()
+
+  navItems.forEach(el => {
+    if (fuzzySearch(val, el.innerText.toLowerCase()) ||
+      fuzzySearch(val, el.getAttribute('data-cat').toLowerCase())) {
+      el.classList.remove('hidden')
+    } else {
+      el.classList.add('hidden')
+    }
+  })
+}
